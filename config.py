99ec30bb7c2ede5b598a37d07bd890ae1257aacd
Kira Krasnova <vaokyn@gmail.com>
2019-07-16 02:19:55 +0300

    Fix config.py

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
 import json
 import os
 
-with open("env.json", "rt") as f:
-    lines = "".join(f.readlines())
-    envs = json.loads(lines)
-
 if "TELEGRAM_TOKEN" in os.environ.keys():
     tg_token = os.environ["TELEGRAM_TOKEN"]
 else:
+    with open("env.json", "rt") as f:
+        lines = "".join(f.readlines())
+        envs = json.loads(lines)
     tg_token = envs["TELEGRAM_TOKEN"]
 
 url = ""
